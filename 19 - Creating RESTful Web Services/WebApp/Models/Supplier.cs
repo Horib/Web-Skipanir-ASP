3ac8180a46f98fcbb4aab3a86179b6977b469a67
Rasmus Hansen <rasmush92@live.com>
2022-08-30 20:52:30 +0100

    19 - Creating RESTful Web Services - 518

---
+++ 19 - Creating RESTful Web Services/WebApp/Models/Supplier.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApp.Models
+{
+    public class Supplier
+    {
+        public long SUpplierId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+
+        public IEnumerable<Product>? Products { get; set; }
+    }
+}
