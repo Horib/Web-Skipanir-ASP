b9c02d4d52b9cdb5507fa1b9d7341595a12d75fc
Rasmus Hansen <rasmush92@live.com>
2022-09-01 11:37:31 +0100

    20 - Advanced Web Services Feature - SuppliersController

---
+++ 20 - Advanced Web Service Features/WebApp/Controllers/SuppliersController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SuppliersController : ControllerBase
+    {
+        private DataContext context;
+
+        public SuppliersController(DataContext ctx)
+        {
+            context = ctx;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<Supplier?> GetSupplier(long id)
+        {
+            Supplier supplier = await context.Suppliers.Include(s => s.Products)
+                .FirstAsync(s => s.SupplierId == id);
+            if (supplier.Products != null)
+            {
+                foreach (Product p in supplier.Products)
+                {
+                    p.Supplier = null;
+                };
+            }
+            return supplier;
+        }
+    }
+}
