d2c1171688a5d93e6d526d2c3ce8daa09b7dc337
Rasmus Hansen <rasmush92@live.com>
2022-09-04 23:51:47 +0100

    21 - Using Controllers with Views, Part I - Creating a Razor View

---
+++ 21 - Using Controllers with Views, Part I/WebApp/Program.cs
@@ -0,0 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using WebApp.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DataContext>(opts =>
+{
+    opts.UseSqlServer(builder.Configuration[
+        "ConnectionStrings:ProductConnection"]);
+    opts.EnableSensitiveDataLogging(true);
+});
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.MapControllers();
+app.MapDefaultControllerRoute();
+
+var context = app.Services.CreateScope().ServiceProvider
+    .GetRequiredService<DataContext>();
+SeedData.SeedDatabase(context);
+
+app.Run();
