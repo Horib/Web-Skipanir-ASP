1c264015188637969f72c1d83d2b882a1beac1dd
Rasmus Hansen <rasmush92@live.com>
2022-09-25 22:04:04 +0100

    30 - Adding FIlters - Done-ish

---
+++ 30 - Using FIlters/WebApp/Filters/SimpleCacheAttribute.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace WebApp.Filters
+{
+    public class SimpleCacheAttribute : Attribute, IAsyncResourceFilter
+    {
+        private Dictionary<PathString, IActionResult> CachedResponses 
+            = new Dictionary<PathString, IActionResult>();
+
+        public async Task OnResourceExecutionAsync(ResourceExecutingContext context,
+            ResourceExecutionDelegate next)
+        {
+            PathString path = context.HttpContext.Request.Path;
+            if (CachedResponses.ContainsKey(path))
+            {
+                context.Result = CachedResponses[path];
+                CachedResponses.Remove(path);
+            } else
+            {
+                ResourceExecutedContext execContext = await next();
+                if (execContext.Result != null)
+                {
+                    CachedResponses.Add(context.HttpContext.Request.Path,
+                        execContext.Result);
+                }
+
+            }
+        }
+
+        public void OnResourceExecuted(ResourceExecutedContext context)
+        {
+            if(context.Result != null)
+            {
+                CachedResponses.Add(context.HttpContext.Request.Path, context.Result);
+            }
+        }
+    }
+}
